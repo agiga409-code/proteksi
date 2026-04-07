b1d3064059a2d55ae210272d9a0af912a10b4c87
agiga409-code <agiga409@gmail.com>
2026-04-07 09:37:53 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBavsJJmRnyV4f-FBQtI4NQgIEJnSvoM50",
+  authDomain: "proteksi-web.firebaseapp.com",
+  databaseURL: "https://proteksi-web-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "proteksi-web",
+  storageBucket: "proteksi-web.firebasestorage.app"
+};
+
